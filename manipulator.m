cf18ff2ab90b013bd1ef5a4fa5cc9e4f4e869499
samlaipolyu <54527198+samlaipolyu@users.noreply.github.com>
2021-04-22 18:02:40 +0800

    Update manipulator.m

--- manipulator.m
+++ manipulator.m
@@ -4,22 +4,26 @@ function [O,min_beta] = manipulator(x,center,obj_rad)
     L = 1:1:50; % mm
     K_A = 0.8550; % K_A = EA/L = 0.8*pi*(4.5^2-1.8^2)/50 = 0.8550 N/mm
     
-    K_B = 5.0211;  % 0.8*313.8175/50 (EI/l=-M/theta_B) Nmmrad-1 //
+   % K_B = 5.0211;  % 0.8*313.8175/50 (EI/l=-M/theta_B) Nmmrad-1 //
     r = 3.15;
     ds_2 = (x(4) + x(5) + x(6))/(3*K_A);
     s_2 = L + ds_2;
     ds_1 = (x(1) + x(2) + x(3)) + ((x(4) + x(5) + x(6)))/(3*K_A);
     s_1 = L + ds_1;
     d = x(7);
+    
+    K_B2 = 0.8*313.8175/s_2(end);
+    K_B1 = 0.8*313.8175/s_1(end);
 
-    theta_2 = (r*s_2(end)/K_B)*sqrt(x(4)^2+x(5)^2+x(6)^2-x(4)*x(5)-x(4)*x(6)-x(5)*x(6));
+    theta_2 = (r*s_2(end)/K_B2)*sqrt(x(4)^2+x(5)^2+x(6)^2-x(4)*x(5)-x(4)*x(6)-x(5)*x(6));
     phi_2 = atan2(3*(x(5)-x(6)),sqrt(3)*(x(5)+x(6)-2*x(4)));
     phi_1 = atan2(3*(x(2)-x(3)),sqrt(3)*(x(2)+x(3)-2*x(1)));
+
     theta_2 = theta_2(1,end);
     A = s_1(end)*r*sqrt(x(1)^2+x(2)^2+x(3)^2-x(1)*x(2)-x(1)*x(3)-x(2)*x(3));
 
-    Theta_1 = (A^2 - 2*cos(phi_1 - phi_2)*A*K_B*theta_2 + K_B^2*theta_2^2)^(1/2)/K_B;
-    Phi_1 = deg2rad(180) - angle(((A*cos(phi_1) - K_B*theta_2*cos(phi_2))*1i)/(A^2 - 2*cos(phi_1 - phi_2)*A*K_B*theta_2 + K_B^2*theta_2^2)^(1/2) + (A*sin(phi_1) - K_B*theta_2*sin(phi_2))/(A^2 - 2*cos(phi_1 - phi_2)*A*K_B*theta_2 + K_B^2*theta_2^2)^(1/2));
+    Theta_1 = (A^2 - 2*cos(phi_1 - phi_2)*A*K_B2*theta_2 + K_B2^2*theta_2^2)^(1/2)/K_B1;
+    Phi_1 = deg2rad(180) - angle(((A*cos(phi_1) - K_B2*theta_2*cos(phi_2))*1i)/(A^2 - 2*cos(phi_1 - phi_2)*A*K_B2*theta_2 + K_B2^2*theta_2^2)^(1/2) + (A*sin(phi_1) - K_B2*theta_2*sin(phi_2))/(A^2 - 2*cos(phi_1 - phi_2)*A*K_B2*theta_2 + K_B2^2*theta_2^2)^(1/2));
 
     theta_1 = Theta_1;
     phi_1 = real(Phi_1);
