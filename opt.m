7578c73148c6547c0f0c16636621d916adf15784
samlaipolyu <54527198+samlaipolyu@users.noreply.github.com>
2022-01-03 20:11:44 +0800

    Update opt.m

--- opt.m
+++ opt.m
@@ -5,8 +5,8 @@ tic
 % 8 
 tspan = 0:deg2rad(3):2*pi;
 z_tip = 110*ones(1,length(tspan));
-x_tip = 40*sign((cos(tspan))).*(sin(tspan)).*cos(tspan).^2;
-y_tip = 40*sign(cos(tspan)).*cos(tspan).^2;
+x_tip = 41*sign((cos(tspan))).*(sin(tspan)).*cos(tspan).^2;
+y_tip = 41*sign(cos(tspan)).*cos(tspan).^2;
 viaPoint = [x_tip; y_tip; z_tip];
 
 % O
@@ -86,7 +86,6 @@ for i = 1:length(viaPoint)
     %     problem = createOptimProblem('fmincon','x0',-[0.02 0.01 0.02 0.02 0.01 0.02 -1]',...
     %         'objective',fun,'lb',-[2; 2; 2; 2; 2; 2; 20],'ub',[-eps; -eps; -eps; -eps; -eps; -eps; 60]);
     %     x(:,i) = run(gs,problem);
-    
     %   fun = @(x)((task-fwk(x(:,end)))'*eye(3)*(task-fwk(x(:,end))) + 2*(norm(x(1:6)-x_prev(1:6))));
     
     fun = @(x)((norm(task-fwk(x(:,end))))^2 +  (0.1)*(norm(x(1:6)-x_prev(1:6)))^2   +...
@@ -94,21 +93,21 @@ for i = 1:length(viaPoint)
         norm(fwkeul_beta(x(1:6,end))  - (set_angle(1,2)))^2  +...
         norm(fwkeul_gamma(x(1:6,end)) - (set_angle(1,3)))^2 );
                    %  (0.1)*norm(manipulator(x,center,obj_rad)).^2   );
-
-    % A = diag((1e-8)*ones(1,7));
-    % b = (1e-2)*ones(7,1);
-    A = [1 1 1 1 1 1 (10^-3)];
-    b = 10^-2;
-    ub = [-eps; -eps; -eps; -eps; -eps; -eps; 60];
-    lb = -[2; 2; 2; 2; 2; 2; 20];
+                   
+    A = [1 1 1 1 1 1 (10^-6)];                            % can be customized
+    b = 10^-4;                                            % can be customized
+    ub = [-eps; -eps; -eps; -eps; -eps; -eps; 60];        % can be customized
+    lb = -[2; 2; 2; 2; 2; 2; 2];                          % can be customized
     
-    option = optimoptions('fmincon','Display','final','Algorithm','interior-point');
+    option = optimoptions('fmincon','Display','final','Algorithm','interior-point'); % or `sqp`
     [x(:,i), fval(i)] = fmincon(fun,x0,A,b,[],[],lb,ub,[],option);   % //'interior-point-convex';
     x_prev = x(:,end);
-    
 end
 
+% correction // optional**
+x = [x(1:6,:);x(7,:)-1];
 toc
+
 %%
 figure(1)
 hold on
@@ -222,22 +221,38 @@ figure(1)
 %%
 %save('x_obj_v1.mat','x')
 
-%%
+%% This part shows the vertically-placed view using the `drawAnimation()` function. %%
+for i = 1:length(x)
+    
+    [pcplot, tip(:,i), sim_angle(:,i)] = drawAnimation(x(:,i),[0 0 0]);
+    hold on;
+    plt = plot3(tip(1,1:i),tip(2,1:i),tip(3,1:i),'r-','linewidth',1);
+    %      FF(i) = getframe(gcf);
+    %      writeVideo(writerObj, FF(end).cdata);
+    hold off
+    
+end
+
+%% Analysis
 s_1_store = [];
 s_2_store = [];
 store_phi_1 = [];
 store_phi_2 = [];
 p_02_prev = [];
+p_00 = zeros(1,3);
+R_00 = eye(3);
+k = 10;
 %
 ax = 80;
 tic
 j = 1;
 fs = figure(2);
 
-while j <= length(x)
-    tic
-    for i = 1:length(x)
 
+while j <= 8*length(x)/8
+    tic
+    for i = 1:8*length(x)/8
+        
         drawnow;
         f_11 = x(1,i);
         f_12 = x(2,i);
@@ -246,61 +261,57 @@ while j <= length(x)
         f_22 = x(5,i);
         f_23 = x(6,i);
         d    = x(7,i);
-     
         L = 1:1:50; % mm
-        % K_A = 0.8550; % K_A = EA/L = 0.8*pi*(4.5^2-1.8^2)/50 = 0.8550 N/mm
-        K_A1 = 0.8*pi*(4.5^2-1.8^2)/(50*(1+0.45*((f_11 + f_12 + f_13 + f_21 + f_22 + f_23))/(0.8*pi*(4.5^2-1.8^2))));
-        K_A2 = 0.8*pi*(4.5^2-1.8^2)/(50*(1+0.45*((f_21 + f_22 + f_23))/(0.8*pi*(4.5^2-1.8^2))));
-        I = (pi/4)*(4.5^4-1.8^4); % 313.8175 mm^4
-        % K_B = 5.0211;  % 0.8*313.8175/50 (EI/l=-M/theta_B) Nmmrad-1 // 0.8MPA
+        
+        F = f_11 + f_12 + f_13 + f_21 + f_22 + f_23;
+        r_o = 9/(10*(- (27*F)/(280*pi) + (((27*F)/(280*pi) - 27/125)^2 - 729/15625)^(1/2) + 27/125)^(1/3)) + (5*((((27*F)/(280*pi) - 27/125)^2 - 729/15625)^(1/2) - (27*F)/(280*pi) + 27/125)^(1/3))/2 + 3/2;
+        r_i =  9/(25*(- (27*F)/(280*pi) + (((27*F)/(280*pi) - 27/125)^2 - 729/15625)^(1/2) + 27/125)^(1/3)) + ((((27*F)/(280*pi) - 27/125)^2 - 729/15625)^(1/2) - (27*F)/(280*pi) + 27/125)^(1/3) + 3/5;
+        K_A1 = 0.8*pi*(r_o^2-r_i^2)/(50*(1+0.45*((f_11 + f_12 + f_13 + f_21 + f_22 + f_23))/(0.8*pi*(r_o^2-r_i^2))));
+        K_A2 = 0.8*pi*(r_o^2-r_i^2)/(50*(1+0.45*((f_21 + f_22 + f_23))/(0.8*pi*(r_o^2-r_i^2))));
         r = 3.15;
-        eta = 1;
         
         ds_2 = (f_21 + f_22 + f_23)/(3*K_A2);
         s_2 = L + ds_2;
         ds_1 = (f_11 + f_12 + f_13 + f_21 + f_22 + f_23)/(3*K_A1);
         s_1 = L + ds_1;
         
-        K_B2 = 0.8*313.8175/s_2(end);
-        K_B1 = 0.8*313.8175/s_1(end);
-
+        K_B2 = 1.0*0.8*313.8175/s_2(end);
+        K_B1 = 1.0*0.8*313.8175/s_1(end);
+        % K_B2 = 0.8*313.8175/50;
+        % K_B1 = 0.8*313.8175/50;
+        
         s_1_store = s_1(end);
         s_2_store = s_2(end);
-
-        theta_2 = eta*(r*s_2_store/K_B2)*sqrt(f_21^2+f_22^2+f_23^2-f_21*f_22-f_21*f_23-f_22*f_23);
+        
+        theta_2 = (r*s_2_store/K_B2)*sqrt(f_21^2+f_22^2+f_23^2-f_21*f_22-f_21*f_23-f_22*f_23);
         phi_2 = atan2(3*(f_22-f_23),sqrt(3)*(f_22+f_23-2*f_21));
-        A = eta*s_1_store*r*sqrt(f_11^2+f_12^2+f_13^2-f_11*f_12-f_11*f_13-f_12*f_13);
+        A = s_1_store*r*sqrt(f_11^2+f_12^2+f_13^2-f_11*f_12-f_11*f_13-f_12*f_13);
         theta_2 = theta_2(end);
-        phi_1 = atan2(3*(f_12-f_13),sqrt(3)*(f_12+f_13-2*f_11));% deg2rad(60);
+        phi_1 = atan2(3*(f_12-f_13),sqrt(3)*(f_12+f_13-2*f_11));
         Theta_1 = (A^2 - 2*cos(phi_1 - phi_2)*A*K_B2*theta_2 + K_B2^2*theta_2^2)^(1/2)/K_B1;
         Phi_1 = deg2rad(180) -  atan2((A*cos(phi_1) - K_B2*theta_2*cos(phi_2))/sqrt(A^2 - 2*cos(phi_1 - phi_2)*A*K_B2*theta_2 + K_B2^2*theta_2^2), (A*sin(phi_1) - K_B2*theta_2*sin(phi_2))/sqrt(A^2 - 2*cos(phi_1 - phi_2)*A*K_B2*theta_2 + K_B2^2*theta_2^2));
         theta_1 = Theta_1;
         phi_1 = real(Phi_1);
         theta_1 = theta_1(end);
-
+        
         % geometry pre-process
         theta_1 = linspace(0,theta_1,50);
         theta_2 = linspace(0,theta_2,50);
-        
-        % geometry pre-process
-        theta_1 = 0:theta_1/(length(L)-1):theta_1;
-        theta_2 = 0:theta_2/(length(L)-1):theta_2;
-        
+
         x_01 = (s_1/theta_1) * (1-cos(theta_1))*cos(phi_1);
         y_01 = (s_1/theta_1) * (1-cos(theta_1))*sin(phi_1);
         z_01 = (s_1/theta_1) * sin(theta_1) + d;
         p_01 = [x_01; y_01; z_01];
-
-        subplot(4,2,[1 3 5 7])
         
-        plot3(x_01,y_01,z_01,'-','LineWidth',4,'color',[0.0265 0.6137 0.8135]);hold on
+        subplot(4,2,[1 3 5 7])
+        plot3(x_01,y_01,z_01,'-','LineWidth',4,'color','[0, 0.4470, 0.7410]');hold on
         xlabel('x [mm]');ylabel('y [mm]');zlabel('z [mm]');
         
-        for i = 1:length(theta_1)
+        for i = 1:1:50
             R_0b{i} = rotz(rad2deg(phi_1-phi_1));
         end
         
-        for i = 1:length(theta_1)
+        for i = 1:1:50
             R_01{i} = rotz(rad2deg(phi_1))*roty(rad2deg(theta_1(1,i)))*rotz(rad2deg(-phi_1));
         end
         
@@ -314,14 +325,13 @@ while j <= length(x)
         z_12 = (s_2/theta_2) * sin(theta_2);
         p_12 = [x_12; y_12; z_12];
         
-        for i = 1:length(theta_2)
+        for i = 1:1:50
             R_12{i} = rotz(rad2deg(phi_2))*roty(rad2deg(theta_2(1,i)))*rotz(rad2deg(-phi_2));
             R_02{i} = R_01{end}*R_12{i};
         end
+        sim_angle(:,j) = transpose(rotm2eul(R_02{50}));
         
-        sim_angle(:,j) = rotm2eul(R_02{1,end},'ZYX');
-        
-        for i = 1:length(R_02)
+        for i = 1:1:50% i = 1:length(R_02)
             p_02(:,i) = (p_01(:,end) + R_01{end}*p_12(:,i));
         end
         
@@ -329,23 +339,20 @@ while j <= length(x)
         y_02 = p_02(2,:);
         z_02 = p_02(3,:);
         
-        plot3(x_02,y_02,z_02,'-','LineWidth',4,'color',[0.9856 0.7372 0.2537]);hold on
+        plot3(x_02,y_02,z_02,'-','LineWidth',4,'color','[0.9290, 0.6940, 0.1250]');hold on
         
+        % Side view
         plot3(-ax*ones(1,length(x_02)),y_02,z_02,'-','LineWidth',4,'color',[0.8 0.8 0.8]);hold on
         plot3(-ax*ones(1,length(x_01)),y_01,z_01,'-','LineWidth',4,'color',[0.7 0.7 0.7]);hold on
         plot3(x_01,ax*ones(1,length(x_01)),z_01,'-','LineWidth',4,'color',[0.7 0.7 0.7]);hold on
         plot3(x_02,ax*ones(1,length(x_02)),z_02,'-','LineWidth',4,'color',[0.8 0.8 0.8]); hold on
-        
-        k = 10;
-        p_00 = [0 0 0];
-        R_00 = eye(3);
         
         rod_x = [p_00, p_00+d*R_00(1,3)];
         rod_y = [p_00, p_00+d*R_00(2,3)];
         rod_z = [p_00, p_00+d*R_00(3,3)];
         plot3(rod_x,rod_y,rod_z,'k-','LineWidth',4);
         plot3(rod_x,ax*ones(1,length(rod_y)),rod_z,'color',[0.6 0.6 0.6],'LineWidth',4);
-        plot3(-ax*ones(1,length(rod_x)),rod_y,rod_z,'color',[0.6 0.6 0.6],'LineWidth',4);
+        plot3(-ax*ones(1,length(rod_x)),-rod_y,rod_z,'color',[0.6 0.6 0.6],'LineWidth',4);
         
         tri_x1_base = [p_00, p_00+k*R_00(1,1)];
         tri_y1_base = [p_00, p_00+k*R_00(2,1)];
@@ -356,9 +363,9 @@ while j <= length(x)
         tri_x3_base = [p_00, p_00+k*R_00(1,3)];
         tri_y3_base = [p_00, p_00+k*R_00(2,3)];
         tri_z3_base = [p_00, p_00+k*R_00(3,3)];
-        plot3(tri_x1_base,tri_y1_base,tri_z1_base,'r-','LineWidth',2);
-        plot3(tri_x2_base,tri_y2_base,tri_z2_base,'g-','LineWidth',2);
-        plot3(tri_x3_base,tri_y3_base,tri_z3_base,'b-','LineWidth',2);
+        plot3(tri_x1_base,tri_y1_base,tri_z1_base,'r-','LineWidth',1);
+        plot3(tri_x2_base,tri_y2_base,tri_z2_base,'g-','LineWidth',1);
+        plot3(tri_x3_base,tri_y3_base,tri_z3_base,'b-','LineWidth',1);
         
         tri_x1_base_0b = [p_0b(1,i), p_0b(1,i)+k*R_0b{i}(1,1)];
         tri_y1_base_0b = [p_0b(2,i), p_0b(2,i)+k*R_0b{i}(2,1)];
@@ -369,9 +376,9 @@ while j <= length(x)
         tri_x3_base_0b = [p_0b(1,i), p_0b(1,i)+k*R_0b{i}(1,3)];
         tri_y3_base_0b = [p_0b(2,i), p_0b(2,i)+k*R_0b{i}(2,3)];
         tri_z3_base_0b = [p_0b(3,i), p_0b(3,i)+k*R_0b{i}(3,3)];
-        plot3(tri_x1_base_0b,tri_y1_base_0b,tri_z1_base_0b,'r-','LineWidth',2);
-        plot3(tri_x2_base_0b,tri_y2_base_0b,tri_z2_base_0b,'g-','LineWidth',2);
-        plot3(tri_x3_base_0b,tri_y3_base_0b,tri_z3_base_0b,'b-','LineWidth',2);
+        plot3(tri_x1_base_0b,tri_y1_base_0b,tri_z1_base_0b,'r-','LineWidth',1);
+        plot3(tri_x2_base_0b,tri_y2_base_0b,tri_z2_base_0b,'g-','LineWidth',1);
+        plot3(tri_x3_base_0b,tri_y3_base_0b,tri_z3_base_0b,'b-','LineWidth',1);
         
         tri_x1_mid = [p_01(1,i), p_01(1,i)+k*R_01{i}(1,1)];
         tri_y1_mid = [p_01(2,i), p_01(2,i)+k*R_01{i}(2,1)];
@@ -382,9 +389,9 @@ while j <= length(x)
         tri_x3_mid = [p_01(1,i), p_01(1,i)+k*R_01{i}(1,3)];
         tri_y3_mid = [p_01(2,i), p_01(2,i)+k*R_01{i}(2,3)];
         tri_z3_mid = [p_01(3,i), p_01(3,i)+k*R_01{i}(3,3)];
-        plot3(tri_x1_mid,tri_y1_mid,tri_z1_mid,'r-','LineWidth',2);
-        plot3(tri_x2_mid,tri_y2_mid,tri_z2_mid,'g-','LineWidth',2);
-        plot3(tri_x3_mid,tri_y3_mid,tri_z3_mid,'b-','LineWidth',2);
+        plot3(tri_x1_mid,tri_y1_mid,tri_z1_mid,'r-','LineWidth',1);
+        plot3(tri_x2_mid,tri_y2_mid,tri_z2_mid,'g-','LineWidth',1);
+        plot3(tri_x3_mid,tri_y3_mid,tri_z3_mid,'b-','LineWidth',1);
         
         tri_x1_tip = [p_02(1,i), p_02(1,i)+k*R_02{i}(1,1)];
         tri_y1_tip = [p_02(2,i), p_02(2,i)+k*R_02{i}(2,1)];
@@ -395,13 +402,13 @@ while j <= length(x)
         tri_x3_tip = [p_02(1,i), p_02(1,i)+k*R_02{i}(1,3)];
         tri_y3_tip = [p_02(2,i), p_02(2,i)+k*R_02{i}(2,3)];
         tri_z3_tip = [p_02(3,i), p_02(3,i)+k*R_02{i}(3,3)];
-        plot3(tri_x1_tip,tri_y1_tip,tri_z1_tip,'r-','LineWidth',2);
-        plot3(tri_x2_tip,tri_y2_tip,tri_z2_tip,'g-','LineWidth',2);
-        plot3(tri_x3_tip,tri_y3_tip,tri_z3_tip,'b-','LineWidth',2);
+        plot3(tri_x1_tip,tri_y1_tip,tri_z1_tip,'r-','LineWidth',1);
+        plot3(tri_x2_tip,tri_y2_tip,tri_z2_tip,'g-','LineWidth',1);
+        plot3(tri_x3_tip,tri_y3_tip,tri_z3_tip,'b-','LineWidth',1);
         p_02_prev = [p_02_prev p_02(:,end)];
-        
+        %
         plot3(p_02_prev(1,1:j),p_02_prev(2,1:j),p_02_prev(3,1:j),'-','linewidth',1,'color',[0.4940 0.1840 0.5560])
-        
+        %
         hold on
         
 %         % Obstacle display
@@ -414,8 +421,7 @@ while j <= length(x)
 %         set(ball, 'FaceAlpha',.51,'EdgeAlpha', 0);
 %         [~,min_beta] = manipulator(x(:,j),center,obj_rad);
 %         minDist(j,:) = min_beta;
-        
-        %figure(2)
+
         plot3(x_tip(1,:),y_tip(1,:),z_tip(1,:),'r-'); hold on
         
         axis equal
@@ -426,9 +432,9 @@ while j <= length(x)
         camproj('perspective')
         view([50.5 31.6]);
         hold off
-    
+        
         figure(2)
-
+        
         if j > 1
             subplot(4,2,2)
             xlim([0 length(x)])
@@ -446,25 +452,24 @@ while j <= length(x)
             subplot(4,2,8)
             xlim([0 length(x)])
             line([j-1 j],[abs(sqrt(x_tip(1,j-1)^2+y_tip(1,j-1)^2+z_tip(1,j-1)^2)-norm(p_02_prev(:,end-1))),...
-                abs(sqrt(x_tip(1,j).^2+y_tip(1,j)^2+z_tip(1,j)^2)-norm(p_02_prev(:,end)))],'Color','b','LineWidth',1); hold on 
+                abs(sqrt(x_tip(1,j).^2+y_tip(1,j)^2+z_tip(1,j)^2)-norm(p_02_prev(:,end)))],'Color','b','LineWidth',1); hold on
             xlabel('step')
-            set(gcf,'color','w');  
+            set(gcf,'color','w');
         end
         j = j + 1;
         set(gcf,'color','w');
-
+        
     end
 end
 
-%%
+%% Check the deviation in 3-axis orientation
 figure(7)
-
 subplot(3,1,1)
 hold on
 plot(rad2deg(sim_angle(1,:)));
 plot(rad2deg(set_angle(1,1)*ones(1,length(sim_angle))),'--');
 xlim([0 length(x)])
-%ylim([-20 20])
+ylim([-20 20])
 box on
 
 subplot(3,1,2)
@@ -472,7 +477,7 @@ hold on
 plot(rad2deg(sim_angle(2,:)));
 plot(rad2deg(set_angle(1,2)*ones(1,length(sim_angle))),'--');
 xlim([0 length(x)])
-%ylim([-20 20])
+ylim([-20 20])
 box on
 
 subplot(3,1,3)
@@ -481,8 +486,6 @@ plot(rad2deg(sim_angle(3,:)));
 plot(rad2deg(set_angle(1,3)*ones(1,length(sim_angle))),'--');
 xlim([0 length(x)])
 box on
-
-legend('\alpha','\beta','\gamma','\alpha_{desired}','\beta_{desired}','\gamma_{desired}')
 
 
 %% collision detection
